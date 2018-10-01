0e4aca8e936bd7687895fda2a0ec3e604adfee33
Stefano Vazzoler <stefanovazzocell@gmail.com>
2018-09-30 19:28:04 -0700

    added ta.update, ta.deleteme and string gen

--- nodeserver/modules/api.js
+++ nodeserver/modules/api.js
@@ -14,6 +14,26 @@ var db;
 var settings;
 var db_ta, db_admin, db_course;
 
+/*
+* genAuth(length)
+* @var length (optional) is length of the secret
+* @return String being random char
+*/
+function getAuth(length = 20) {
+	// Removed: I l ? / : @ - . _ ~ ! $ & ' ( ) * + , ; =
+	// Length 
+	var pool = 'ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz0123456789';
+	var rnd = crypto.randomBytes(length);
+	var value = new Array(length);
+	var lim = 256 / pool.length;
+
+	for (var i = 0; i < length; i++) {
+		  value[i] = pool[Math.floor(rnd[i] / lim)];
+	}
+
+	return value.join('');
+}
+
 // Making public functions available
 module.exports = {
 	/*
@@ -62,19 +82,44 @@ module.exports = {
 		// Saves user data
 		push: function (req, res) {
 			if (!((req.body.calendar) instanceof Array) || (req.body.calendar).length !== ((settings['fromto'][1] - settings['fromto'][0]) * 7 * 2)) {
-				console.log('API / ta.push / Failed calendar check 1');
+				console.log('API / ta.push / Failed check 1');
 				res.status(400);
 				res.send();
 				return false;
 			}
 			for (let i = 0; i < (req.body.calendar).length; i++) {
 				if ((req.body.calendar)[i] !== 0 && (req.body.calendar)[i] !== 1 && (req.body.calendar)[i] !== 2) {
+					console.log('API / ta.push / Failed check 2');
 					res.status(400);
 					res.send();
 					return false;
 				}
 			}
-			db_ta.updateOne({ 'auth': String(req.body.auth) }, {$set: { calendar: req.body.calendar }},
+			let time = new Date().getTime();
+			db_ta.updateOne({ 'auth': String(req.body.auth) },
+				{$set: { calendar: req.body.calendar, lastPush: time }},
+				function(err, result) {
+					if (err) throw err;
+					if (result) {
+						res.send(getAuth());
+					} else {
+						res.status(500);
+						res.send();
+					}
+				});
+		},
+		// Updates user info
+		update: function (req, res) {
+			if ((String(req.body.username) < 2 || String(req.body.username) > 40) || 
+				(String(req.body.email) < 5 || String(req.body.email) > 200) ||
+				(req.body.privacy !== 0 && req.body.privacy !== 1 && req.body.privacy !== 2)) {
+				console.log('API / ta.update / Failed check');
+				res.status(400);
+				res.send();
+				return false;
+			}
+			db_ta.updateOne({ 'auth': String(req.body.auth) },
+				{$set: { name: String(req.body.username), email: String(req.body.email), privacy: req.body.privacy }},
 				function(err, result) {
 					if (err) throw err;
 					if (result) {
@@ -85,18 +130,17 @@ module.exports = {
 					}
 				});
 		},
-		// Updates user info
-		update: function (req, res) {
-			res.send('OK');
-			// TODO
-		},
 		// Removes user
 		deleteme: function (req, res) {
-			res.send('OK');
-			// TODO
+			db_ta.deleteOne({ 'auth': String(req.body.auth) },
+				function (err, obj) {
+					if (err) throw err;
+					res.send();
+				});
 		},
 		// Resets auth
 		resetauth: function (req, res) {
+			let new = getAuth()
 			res.send('OK');
 			// TODO
 		},
