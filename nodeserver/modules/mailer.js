f5876ad081b06954e2b8aee05d8cfa46b1741b74
Stefano Vazzoler <stefanovazzocell@gmail.com>
2018-09-30 22:03:54 -0700

    added demo mode

--- nodeserver/modules/mailer.js
+++ nodeserver/modules/mailer.js
@@ -10,28 +10,41 @@
 * Require Node dependencies
 */
 
-const { mailgun_api, domain, from, webmaster } = require('./../config/mailer');
+const { mailgun_api, domain, from, webmaster, siteDomain, demoMode } = require('./../config/mailer');
 var mailer;
 var template;
 
 module.exports = {
 	/* setup(Mailgun) - sets up mailer */
 	setup: function (Mailgun) {
-		console.log();
-		mailer = new Mailgun( {apiKey: mailgun_api, domain: domain} );
+		if (demoMode) {
+			console.warn('MAILER / send / Careful, is demo mode!');
+			mailer = new Mailgun( {apiKey: mailgun_api, domain: domain} );
+		}
 		var fs = require('fs');
-		template = fs.readFileSync('./../config/mailer/email_template.html', 'utf8');
+		template = fs.readFileSync('./config/email_template.html', 'utf8');
 		template = template.replace(':webmaster:', webmaster);
 		console.log('Mailer setup completed');
 	},
-	/* send(sendfrom, sendto, subject, message) - Sends an email */
-	send: function (sendfrom, sendto, subject, message) {
-		console.log(template.replace(':subject:',subject).replace(':message:',message)); // TODO: Remove this
-		/*
-		mailer.messages().send({ from: (sendfrom + from), to: sendto, subject: subject, html: message },
+	/* send(sendfrom, sendto, subject, message, callbackFn) - Sends an email */
+	send: function (sendfrom, sendto, subject, message, callbackFn) {
+		let messageHtml = template.replace(/:subject:/g,subject).replace(/:message:/g,message).replace(/:domain:/g,siteDomain);
+		if (demoMode) {
+			console.log('MAILER / send / demo mode');
+			console.log('----------');
+			console.log('from: ' + (sendfrom + from));
+			console.log('to: ' + sendto);
+			console.log('subject: ' + subject);
+			console.log('----------');
+			console.log(message);
+			console.log('----------');
+			callbackFn();
+		} else {
+			mailer.messages().send({ from: (sendfrom + from), to: sendto, subject: subject, html: messageHtml, text: message },
 			function (err, body) {
 				if (err) throw err;
+				callbackFn();
 			});
-		*/
+		}
 	}
 }
