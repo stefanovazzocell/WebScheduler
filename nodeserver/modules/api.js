b2a299f61aeac35388e4bf41173e4337c264e031
Stefano Vazzoler <stefanovazzocell@gmail.com>
2018-09-30 17:39:17 -0700

    added push

--- nodeserver/modules/api.js
+++ nodeserver/modules/api.js
@@ -11,14 +11,16 @@
 */
 
 var db;
+var settings;
 var db_ta, db_admin, db_course;
 
 // Making public functions available
 module.exports = {
 	/*
-	* setup(dburl, mClient, callback) - setup db with given url and given mongoclient, then call callback
+	* setup(dburl, mClient, givenSettings, callback) - setup db with given url and given mongoclient, then call callback
 	*/
-	setup: function (dburl, mClient, callback) {
+	setup: function (dburl, mClient, givenSettings, callback) {
+		settings = givenSettings;
 		mClient.connect(dburl, { useNewUrlParser: true }, (err, client) => {
 			if (err) {
 				throw err;
@@ -59,8 +61,29 @@ module.exports = {
 		},
 		// Saves user data
 		push: function (req, res) {
-			res.send('OK');
-			// TODO
+			if (!((req.body.calendar) instanceof Array) || (req.body.calendar).length !== ((settings['fromto'][1] - settings['fromto'][0]) * 7 * 2)) {
+				console.log('API / ta.push / Failed calendar check 1');
+				res.status(400);
+				res.send();
+				return false;
+			}
+			for (let i = 0; i < (req.body.calendar).length; i++) {
+				if ((req.body.calendar)[i] !== 0 && (req.body.calendar)[i] !== 1 && (req.body.calendar)[i] !== 2) {
+					res.status(400);
+					res.send();
+					return false;
+				}
+			}
+			db_ta.updateOne({ 'auth': String(req.body.auth) }, {$set: { calendar: req.body.calendar }},
+				function(err, result) {
+					if (err) throw err;
+					if (result) {
+						res.send();
+					} else {
+						res.status(500);
+						res.send();
+					}
+				});
 		},
 		// Updates user info
 		update: function (req, res) {
